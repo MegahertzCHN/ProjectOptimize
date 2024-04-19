95b1d775ecedb05e78fd0c5f9fb85fd7d11d0cb8
chill <chill@Chills-MacBook-Pro.local>
2024-04-19 11:53:28 +0800

    如何记录项目的启动时间

---
+++ Optimize-LaunchTime/Optimize-LaunchTime/AppLaunchTime.h
@@ -0,0 +1,18 @@
+//
+//  AppLaunchTime.h
+//  Optimize-LaunchTime
+//
+//  Created by Chill on 2024/4/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppLaunchTime : NSObject
+
++ (void)mark;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Optimize-LaunchTime/Optimize-LaunchTime/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Optimize-LaunchTime
+//
+//  Created by Chill on 2024/4/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
